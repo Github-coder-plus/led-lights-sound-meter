2a07e2230797455d5fcd9c749afd30fb83a52f7a
Github-coder-plus <181423861+Github-coder-plus@users.noreply.github.com>
2025-12-04 10:21:44 -0700

    Added code comments

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
+// Basic sound checking script made by Git-coder-plus
 basic.forever(function () {
-    led.plotBarGraph(
-        input.soundLevel(),
+    led.plotBarGraph( // as of current plots on micro:bit will add to led strip
+        input.soundLevel(), // Detects sound
         255
     )
 })
