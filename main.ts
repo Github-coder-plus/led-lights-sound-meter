105ea72f0d57731a309cba73f4c1f703b5fb4417
0T5 <161741261+0xTex5@users.noreply.github.com>
2025-12-04 10:36:01 -0700

    strip

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
-// Basic sound checking script made by Git-coder-plus
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+strip.showBarGraph(0, 255)
 basic.forever(function () {
-    led.plotBarGraph( // as of current plots on micro:bit will add to led strip
-        input.soundLevel(), // Detects sound
+    /*led.plotBarGraph(
+        input.soundLevel(),
         255
-    )
+    )*/
+    strip.showBarGraph(
+        input.soundLevel(),
+        255
+    );
 })
 
