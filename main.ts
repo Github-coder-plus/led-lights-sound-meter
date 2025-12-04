d27e4ebc962109fa535f419760f219b10b1c5d90
0T5 <161741261+0xTex5@users.noreply.github.com>
2025-12-04 10:39:59 -0700

    code comments

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
+//led connectivity issues but this creates the strip with the set pixels
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
-strip.showBarGraph(0, 255)
+//forever does this
 basic.forever(function () {
     led.plotBarGraph(
         input.soundLevel(),
         255
-    )
+    );
+    //plots the sound on led up to 255, records sound level
     strip.showBarGraph(
         input.soundLevel(),
         255
     );
+    //shows the graph on strip
 })
 
